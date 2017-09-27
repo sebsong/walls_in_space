0c0de4b11d5e497675caafac044071e9c646a9c7
Sebastian Song <sebsong@Sebastians-MacBook-Pro.local>
2017-09-27 01:35:19 -0700

    added some boost functionarlity

--- Assets/scripts/ShipController.cs
+++ Assets/scripts/ShipController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class ShipController : MonoBehaviour {
 	public float spinSpeed;
 	Rigidbody rb;
+	float boost;
+	bool boostReady;
+	bool isBoosting;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		boost = 0f;
+		boostReady = false;
+		isBoosting = false;
 	}
 	
 	// Update is called once per frame
@@ -17,6 +23,10 @@ public class ShipController : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.R)) {
 			Respawn ();
+		}
+
+		if (boostReady && Input.GetKeyDown (KeyCode.Space)) {
+			StartCoroutine ("Boost");
 		}
 	}
 
@@ -28,14 +38,47 @@ public class ShipController : MonoBehaviour {
 		transform.Rotate (rotDir * spinSpeed * Time.deltaTime, 0, 0);
 	}
 
-	void OnCollisionEnter(Collision coll) {
+	void AddBoost (float boostVal) {
+		if (boost < 100f) {
+			float newBoost = boost + boostVal;
+			if (newBoost >= 100f) {
+				boost = 100f;
+				boostReady = true;
+			} else {
+				boost = newBoost;
+			}
+		}
+	}
+
+	IEnumerator Boost () {
+		isBoosting = true;
+		rb.isKinematic = true;
+		float speed = 0f;
+		while (isBoosting) {
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+			speed += 5f;
+			yield return null;
+		}
+	}
+
+	void OnCollisionEnter (Collision coll) {
 		rb.useGravity = true;
 	}
 
+	void OnTriggerEnter (Collider coll) {
+		AddBoost (1f);
+	}
+
 	void Respawn () {
+		boostReady = false;
+		isBoosting = false;
+
+		rb.isKinematic = false;
 		rb.useGravity = false;
+
 		rb.velocity = Vector3.zero;
 		rb.angularVelocity = Vector3.zero;
+
 		transform.position = Vector3.zero;
 		transform.rotation = Quaternion.Euler (0, 90, 0);
 	}
--- Assets/scripts/WallController.cs
+++ Assets/scripts/WallController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallController : MonoBehaviour {
+public abstract class WallController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {

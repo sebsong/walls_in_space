3480f530d049d2b4e07566add7d8c453eda6b3f3
Sebastian Song <sebsong@berkeley.edu>
2017-10-03 15:04:29 -0700

    added text blink

---
+++ Assets/scripts/TextBlink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBlink : MonoBehaviour {
+	Text t;
+	string original;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Text> ();
+		original = t.text;
+		StartCoroutine ("Blink");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Blink () {
+		while (true) {
+			yield return new WaitForSeconds (.75f);
+			t.text = "";
+			yield return new WaitForSeconds (.75f);
+			t.text = original;
+		}
+	}
+}
